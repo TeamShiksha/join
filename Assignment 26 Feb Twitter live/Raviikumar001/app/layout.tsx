79e8550036f27b28735db1b5ec3b2dbb6844c5af
Ravi <samcollins9899@gmail.com>
2025-02-26 23:37:21 +0530

    chore: refactore metadata

--- Assignment 26 Feb Twitter live/Raviikumar001/app/layout.tsx
+++ Assignment 26 Feb Twitter live/Raviikumar001/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'book store',
+  description: 'library book',
+  generator: 'book',
 }
 
 export default function RootLayout({
