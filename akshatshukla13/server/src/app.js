d527806de623db89592a197bfa77a8802273e102
Akshat-Sh <akshatvijay1302@gmail.com>
2025-02-26 23:21:03 +0530

    Configure CORS to allow requests from the client application

--- akshatshukla13/server/src/app.js
+++ akshatshukla13/server/src/app.js
@@ -3,7 +3,11 @@ import bookRoutes from "./routes/routes.js";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://join-client-murex.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use("/books", bookRoutes);
 
