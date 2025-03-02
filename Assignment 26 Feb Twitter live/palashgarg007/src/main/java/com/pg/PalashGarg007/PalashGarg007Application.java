f8d57a78ee1f333d78fa135b02e103d76fe83633
Palash garg <gargpalash71@gmail.com>
2025-03-02 08:31:56 +0530

    ReWork
    Discarded all the previous changes and added the project again from scratch.

---
+++ Assignment 26 Feb Twitter live/palashgarg007/src/main/java/com/pg/PalashGarg007/PalashGarg007Application.java
@@ -0,0 +1,13 @@
+package com.pg.PalashGarg007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalashGarg007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalashGarg007Application.class, args);
+	}
+
+}
