5851a7957f4e95b8ee32cbe9cd90d65f4eb86ede
Pratish Ninawe <pratish.1006@gmail.com>
2025-05-03 23:10:36 +0530

    Image processing UI Frontend
    Signed-off-by: Pratish Ninawe <pratish.1006@gmail.com>

---
+++ Assignment 3 May Twitter live/Pratish10/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import FileUpload from "./Components/FileUpload";
+
+function App() {
+  return (
+    <div className="text-2xl">
+      <FileUpload />
+    </div>
+  );
+}
+
+export default App;
