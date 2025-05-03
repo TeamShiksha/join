e1d48323768aac6529ae5d0d15c7378fc1185af1
ShlokMane <shlokm25@gmail.com>
2025-05-03 23:00:08 +0530

    Complete Assignment

---
+++ Assignment 3 May Twitter live/ShlokMane/src/main/java/shiksha/team/demo/TeamSikhshaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package shiksha.team.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSikhshaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamSikhshaAssignmentApplication.class, args);
+    }
+
+}
---
+++ Assignment 3 May Twitter live/ShlokMane/src/test/java/shiksha/team/demo/TeamSikhshaAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.shiksha.team.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamSikhshaAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
