55c7f8400da1692851b5b7e76eef756216b3e265
Ankit Pandey <ankit221601@gmail.com>
2025-05-09 02:37:40 +0530

    updated

--- Assignment 3 May Twitter live/helloankitpandey/assignment/src/App.jsx
+++ Assignment 3 May Twitter live/helloankitpandey/assignment/src/App.jsx
@@ -142,7 +142,7 @@ function App() {
                 download={`processed-${idx + 1}.png`}
                 className="block mt-4 text-center bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition-colors"
               >
-                You can Download Image here
+                Download
               </a>
             </div>
           </div>
